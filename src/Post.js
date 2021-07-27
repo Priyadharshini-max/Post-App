11864f146b49df9bc9d43c3a88863101ed38b277
Priyadharshini-max <59358875+Priyadharshini-max@users.noreply.github.com>
2021-07-27 12:52:23 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Container,Table,Button } from "react-bootstrap";
+import { Container, Table, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const API_URL = "https://jsonplaceholder.typicode.com/posts";
@@ -16,18 +16,17 @@ class PostComponent extends React.Component {
       body: "",
       username: [],
       value: "",
-      show: false,
-      display: false
     };
   }
 
+  // Post and User Uploaded in UI ( componentDidMount() is invoked immediately after a component is mounted )
   componentDidMount = () => {
     this.getPosts();
     this.getUsers();
   };
 
+  //Fetch data from API and update post using setState
   getPosts = async () => {
-
     try {
       const { data } = await axios.get(API_URL);
       this.setState({ posts: data });
@@ -36,8 +35,8 @@ class PostComponent extends React.Component {
     }
   };
 
+  //Create new Post and Push new Post to posts array
   createPost = async () => {
-
     try {
       const { userId, title, body } = this.state;
       const { data } = await axios.post(API_URL, {
@@ -53,8 +52,8 @@ class PostComponent extends React.Component {
     }
   };
 
+  //Update Post
   updatePost = async () => {
-
     try {
       const { id, userId, title, body, posts } = this.state;
       const { data } = await axios.put(`${API_URL}/${id}`, {
@@ -65,14 +64,14 @@ class PostComponent extends React.Component {
       const index = posts.findIndex((post) => post.id === id);
       posts[index] = data;
 
-      this.setState({ posts, id: "", userId: "", title: "", body: ""});
+      this.setState({ posts, id: "", userId: "", title: "", body: "" });
     } catch (err) {
       console.log(err);
     }
   };
 
+  //Delete post
   deletePost = async (postId) => {
-
     try {
       await axios.delete(`${API_URL}/${postId}`);
 
@@ -85,12 +84,15 @@ class PostComponent extends React.Component {
     }
   };
 
+  //Display data in Input box
   selectPost = (post) => this.setState({ ...post });
+
 
   handleChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value });
   };
 
+  // Form submit 
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitted...");
@@ -101,6 +103,7 @@ class PostComponent extends React.Component {
     }
   };
 
+  //Display user name in select option
   getUsers = async () => {
     try {
       var { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
@@ -113,106 +116,98 @@ class PostComponent extends React.Component {
     }
   }
 
-  GetID = () => {
-    console.log("GetID.......");
-  }
-
-  handleClose = () => {
-    this.setState({ show: false });
-  }
-
+  // Display user ID in UserID input box
   FetchID = (event) => {
     this.setState({ userId: event.target.value })
     console.log(event.target.value)
   }
 
-  handleClose = () => {
-    this.setState({ show: false })
-    this.setState({ display: false })
-  }
   render() {
- 
+    //Props for goBack and goForward
     const { history } = this.props;
     return (
       <>
-      <i onClick={history.goBack} style={{  fontSize: "30px" }} className="fas fa-arrow-left"></i>
+        {/* goBack and goForward Icons */}
+
+        <i onClick={history.goBack} style={{ fontSize: "30px" }} className="fas fa-arrow-left"></i>
         <i onClick={history.goForward} style={{ float: "right", fontSize: "30px" }} className="fas fa-arrow-right"></i>
-        <Container className = "PostContainer">
-        <form onSubmit={this.handleSubmit}>
-          <label>UserName : </label>
-          <select onChange={this.FetchID}  className="username">
-            <option>Select UserName</option>
-            {this.state.username.map((name) => {
-              return (
-                <option value={name.id}>{name.name}</option>
-              )
-            })}
-          </select><br />
+        <Container className="PostContainer">
 
-          <label> UserID : </label>
-          <input
-            type="number"
-            name="userId"
-            className="userid"
-            value={this.state.userId}
-            onChange={this.handleChange}
-          /><br />
-          <label> Title : </label>
-          <input
-            type="text"
-            name="title"
-            className="title"
-            value={this.state.title}
-            onChange={this.handleChange}
-          /><br />
-          <label> Body : </label>
-          <input
-            type="text"
-            name="body"
-            className="body"
-            value={this.state.body}
-            onChange={this.handleChange}
-          />
-          <input type="Submit" />
-        </form>
-
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>UserId</th>
-              <th>Title</th>
-              <th>Body</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map((post) => {
-              return (
-                <tr key={post.id}>
-                  <td>{post.id}</td>
-                  <td>{post.userId}</td>
-                  <td>{post.title}</td>
-                  <td>{post.body}</td>
-                  <td>
-                    <Button size="sm" onClick={() => this.selectPost(post)}>
-                      Edit
-                    </Button><br />
-                    <Button
-                      variant="danger"
-                      size="sm"
-                      onClick={() => this.deletePost(post.id)}
-                    >
-                      Delete
-                    </Button>
-                    <Link to={`/getuser/${post.userId}`}><Button type="button" variant="secondary" size="sm">Getuser</Button></Link>
-                    <Link to={`/getcomments/${post.userId}`}><Button type="button" size="sm">GetComments</Button></Link>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+          {/* Form to create new user and update existing user */}
+          <form onSubmit={this.handleSubmit}>
+            <label>UserName : </label>
+            <select onChange={this.FetchID} className="username">
+              <option>Select UserName</option>
+              {this.state.username.map((name) => {
+                return (
+                  <option value={name.id}>{name.name}</option>
+                )
+              })}
+            </select><br />
+            <label> UserID : </label>
+            <input
+              type="number"
+              name="userId"
+              className="userid"
+              value={this.state.userId}
+              onChange={this.handleChange}
+            /><br />
+            <label> Title : </label>
+            <input
+              type="text"
+              name="title"
+              className="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            /><br />
+            <label> Body : </label>
+            <input
+              type="text"
+              name="body"
+              className="body"
+              value={this.state.body}
+              onChange={this.handleChange}
+            />
+            <input type="Submit" />
+          </form>
+          {/* Table to display all posts */}
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>UserId</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.posts.map((post) => {
+                return (
+                  <tr key={post.id}>
+                    <td>{post.id}</td>
+                    <td>{post.userId}</td>
+                    <td>{post.title}</td>
+                    <td>{post.body}</td>
+                    <td>
+                      <Button size="sm" onClick={() => this.selectPost(post)}>
+                        Edit
+                      </Button><br />
+                      <Button
+                        variant="danger"
+                        size="sm"
+                        onClick={() => this.deletePost(post.id)}
+                      >
+                        Delete
+                      </Button>
+                      <Link to={`/getuser/${post.userId}`}><Button type="button" variant="secondary" size="sm">Getuser</Button></Link>
+                      <Link to={`/getcomments/${post.userId}`}><Button type="button" size="sm">GetComments</Button></Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
         </Container>
       </>
     );
