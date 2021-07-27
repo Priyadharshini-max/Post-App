910ce02068415abfc51dc49cdbfeeaa95dd12fa8
Priyadharshini-max <59358875+Priyadharshini-max@users.noreply.github.com>
2021-07-27 12:58:53 +0530

    Update GetComments.js

--- src/GetComments.js
+++ src/GetComments.js
@@ -9,41 +9,43 @@ class GetComments extends Component {
       comments: [],
     }
   }
+
+  // Get PostId and fetch data from API
   GetComments = async (props) => {
     const history = this.props;
     const postId = history.match.params.userId;
-    console.log("userId2", postId);
-
     try {
       var { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
       this.setState({ comments: data })
-      console.log(data);
-      console.log(this.state.comments);
     } catch (err) {
       console.log(err);
     }
     console.log("GetComments");
   }
 
+  //componentDidMount() is invoked immediately after a component is mounted
   componentDidMount = () => {
     this.GetComments();
   }
 
   render() {
+    //Props for goBack 
     const { history } = this.props
     return (
       <>
+        {/* goBack */}
         <i onClick={history.goBack} style={{ float: "left", fontSize: "30px" }} className="fas fa-arrow-left"></i>
         <Container>
           <h1>Comments</h1>
           {this.state.comments.map((comment, index) => {
             return (
-              <Table  striped bordered hover>
+              // Table to display all comments 
+              <Table striped bordered hover>
                 <thead>
-                  <tr key={index}><strong>{index+1})  Name : </strong>{comment.name}<br />
-                  <strong>Email : </strong>{comment.email}<br />
-                  <strong>Body : </strong>{comment.body}
-                   </tr>                  
+                  <tr key={index}><strong>{index + 1})  Name : </strong>{comment.name}<br />
+                    <strong>Email : </strong>{comment.email}<br />
+                    <strong>Body : </strong>{comment.body}
+                  </tr>
                 </thead>
               </Table>
             )
